e38fef44cf1a57cc465877e5d2f6bd61d253bde3
tac3563 <tcox_bu@outlook.com>
2025-08-10 12:41:45 +0100

    [Update] initial setup of tailwind design system

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+export default function Nav() {
+    return (
+        <>
+            <nav id='nav' className='nav-menu'>
+                <div className="nav-menu-wrapper">
+                    <div className="nav-logo"><a href="#">Cottage Retreat</a></div>
+                    <ul className='nav-menu-links'>
+                        <li></li>
+                    </ul>
+                </div>
+
+            </nav>
+        </>
+    )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <main id="main">
+                <h1 className="typo-heading-l">This is the homepage</h1>
+            </main>
+        </>
+    );
+}
